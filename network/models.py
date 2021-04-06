f7f675845fba933f7f6928ea5592ac11df3d8d9c
urancon <ulysse.rancon@gmail.com>
2021-04-06 09:26:47 +0200

    removed old pure CNN SpikeFlowNetLike, added binocular pure SNN FusionFlowNetLike class

--- network/models.py
+++ network/models.py
@@ -1,11 +1,12 @@
+import math
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import transforms
 from spikingjelly.clock_driven import functional, layer, surrogate  # , accelerating
 from spikingjelly.clock_driven.neuron import BaseNode, IFNode, LIFNode
-# from test_blocks import PLIFNode
-from torchvision import transforms
-import math
+# from blocks import PLIFNode
 
 
 class NeuromorphicNet(nn.Module):
@@ -39,7 +40,7 @@ class DepthSNN(NeuromorphicNet):
         pass
 
 
-class MonoSpikeFlowNetLike(nn.Module):
+class SpikeFlowNetLike(nn.Module):
     """
     A full spiking Spike-FlowNet network, but for monocular depth estimation.
     Its architecture is very much like a U-Net, and it outputs a single-channel depth map of the same size as the input
@@ -55,12 +56,12 @@ class MonoSpikeFlowNetLike(nn.Module):
         else:
             return input_tensor[:, :, :target.size(2), :target.size(3)]
 
-    def __init__(self, tau, T, v_threshold=1.0, v_reset=0.0, v_infinite_thresh=1000):
+    def __init__(self, tau, T, v_threshold=1.0, v_reset=0.0, v_infinite_thresh=float('inf')):
         super().__init__()
         # SNN-related parameters
         self.T = T
 
-        # encoder layers
+        # encoder layers (downsampling)
         self.conv1 = nn.Sequential(
             nn.Conv2d(in_channels=2, out_channels=64, kernel_size=3, stride=2, padding=(3-1)//2, bias=False),
             nn.BatchNorm2d(64),
@@ -104,68 +105,43 @@ class MonoSpikeFlowNetLike(nn.Module):
             LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
         )
 
-        # decoder layers
-        # (upsampling layers)
+        # decoder layers (upsampling)
+        self.deconv4 = nn.Sequential(
+            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
         self.deconv3 = nn.Sequential(
-            nn.ConvTranspose2d(512, 128, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(128),
             LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
         )
         self.deconv2 = nn.Sequential(
-            nn.ConvTranspose2d(256+128+1, 64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(64),
             LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
         )
         self.deconv1 = nn.Sequential(
-            nn.ConvTranspose2d(128+64+1, 4, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(4),
+            nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(1),
             LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
         )
 
         # these layers output depth maps at different scales, where depth is represented by the potential of IF neurons
         # that do not fire ("I-neurons"), i.e., with an infinite threshold.
-        infinite_thresh = 1000
-        self.predict_depth4 = nn.Sequential(
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 1, kernel_size=1, stride=1, padding=0, bias=False),
-            IFNode(v_threshold=v_infinite_thresh, v_reset=v_reset, surrogate_function=surrogate.ATan()),
-        )
-        self.predict_depth3 = nn.Sequential(
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 1, kernel_size=1, stride=1, padding=0, bias=False),
-            IFNode(v_threshold=v_infinite_thresh, v_reset=v_reset, surrogate_function=surrogate.ATan()),
-        )
-        self.predict_depth2 = nn.Sequential(
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 1, kernel_size=1, stride=1, padding=0, bias=False),
-            IFNode(v_threshold=v_infinite_thresh, v_reset=v_reset, surrogate_function=surrogate.ATan()),
-        )
-        self.predict_depth1 = nn.Sequential(
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 1, kernel_size=1, stride=1, padding=0, bias=False),
+        self.predict_depth = nn.Sequential(
             IFNode(v_threshold=v_infinite_thresh, v_reset=v_reset, surrogate_function=surrogate.ATan()),
         )
 
-        self.upsampled_depth4_to_3 = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=512, out_channels=32, kernel_size=4, stride=2, padding=1, bias=False),
-            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
-        )
-        self.upsampled_depth3_to_2 = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=384+1, out_channels=32, kernel_size=4, stride=2, padding=1, bias=False),
-            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
-        )
-        self.upsampled_depth2_to_1 = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=192+1, out_channels=32, kernel_size=4, stride=2, padding=1, bias=False),
-            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
-        )
-        self.upsampled_depth1_to_0 = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=68+1, out_channels=32, kernel_size=4, stride=2, padding=1, bias=False),
-            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
-        )
-
+    def detach(self):
+        for m in self.modules():
+            if isinstance(m, BaseNode):
+                m.v.detach_()
+            elif isinstance(m, layer.Dropout):
+                m.mask.detach_()
 
     def forward(self, x):
-        # TODO: in Spike-FlowNet, "the loss is evaluated after forward-propagating all consecutive input event frames
+        #  TODO: in Spike-FlowNet, "the loss is evaluated after forward-propagating all consecutive input event frames
         #   within the time window"
 
         # TODO: for our depth prediction task on MVSEC, membrane potentials should NOT be reset
@@ -175,10 +151,14 @@ class MonoSpikeFlowNetLike(nn.Module):
         # TODO: on my personal computer, too much memory is needed for a simple inference on 1 frame with T=100 !!! we
         #  need to know the memory limitations of our data representation
 
-        for t in range(self.T):
+        # x must be of shape [batch_size, num_frames_per_depth_map, 2 (polarities), W, H]
+
+        for t in range(x.shape[1]):
+
+            frame = x[:, t, :, :, :]
 
             # pass through encoder layers
-            out_conv1 = self.conv1(x[t])
+            out_conv1 = self.conv1(frame)
             out_conv2 = self.conv2(out_conv1)
             out_conv3 = self.conv3(out_conv2)
             out_conv4 = self.conv4(out_conv3)
@@ -190,43 +170,189 @@ class MonoSpikeFlowNetLike(nn.Module):
             out_rconv22 = self.conv_r22(out_rconv21) + out_rconv12
 
             # gradually upsample while concatenating and passing through skip connections
-            depth4 = self.predict_depth4(self.upsampled_depth4_to_3(out_rconv22))
-            depth4_up = self.crop_like(depth4, out_conv3)
-            out_deconv3 = self.crop_like(self.deconv3(out_rconv22), out_conv3)
-            concat3 = torch.cat((out_conv3, out_deconv3, depth4_up), 1)
+            out_deconv4 = self.deconv4(out_rconv22)
+            out_add4 = self.crop_like(out_deconv4, out_conv3) + out_conv3
 
-            depth3 = self.predict_depth3(self.upsampled_depth3_to_2(concat3))
-            depth3_up = self.crop_like(depth3, out_conv2)
-            out_deconv2 = self.crop_like(self.deconv2(concat3), out_conv2)
-            concat2 = torch.cat((out_conv2, out_deconv2, depth3_up), 1)
+            out_deconv3 = self.deconv3(out_add4)
+            out_add3 = self.crop_like(out_deconv3, out_conv2) + out_conv2
 
-            depth2 = self.predict_depth2(self.upsampled_depth2_to_1(concat2))
-            depth2_up = self.crop_like(depth2, out_conv1)
-            out_deconv1 = self.crop_like(self.deconv1(concat2), out_conv1)
-            concat1 = torch.cat((out_conv1, out_deconv1, depth2_up), 1)
+            out_deconv2 = self.deconv2(out_add3)
+            out_add2 = self.crop_like(out_deconv2, out_conv1) + out_conv1
 
-            depth1 = self.predict_depth1(self.upsampled_depth1_to_0(concat1))
+            out_deconv1 = self.deconv1(out_add2)
 
-        return depth1
+            # the membrane potentials of the output IF neuron carry the depth prediction
+            out_depth1 = self.predict_depth(out_deconv1)
+            return self.predict_depth[-1].v
 
 
-if __name__ == "__main__":
-    # from mvsec_utils.mvsec_dataset import MVSEC
+class FusionFlowNetLike(nn.Module):
+    """
+    A fully spiking Fusion-FlowNet network, but for binocular depth estimation.
+    Its architecture is very much like a U-Net, and it outputs a single-channel depth map of the same size as the input
+    field of view. The main difference with a U-Net is in the encoder, which takes as inputs 2 DVS spike trains.
+    Basically, we have replaced all ReLU activation functions in Fusion-FlowNet by LIFNodes -> non-linearity.
+    The stream of spikes of the second DVS camera replaces the grayscale image that is fed to the network in
+    Fusion-FlowNet. Concatenations are also replaced by additions.
+    Intermediate depth maps (see paper) come out of IFNodes with an infinite threshold.
+    """
 
-    #device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
-    device = 'cpu'
+    @staticmethod
+    def crop_like(input_tensor, target):
+        if input_tensor.size()[2:] == target.size()[2:]:
+            return input_tensor
+        else:
+            return input_tensor[:, :, :target.size(2), :target.size(3)]
 
-    T = 50
+    def __init__(self, tau, T, v_threshold=1.0, v_reset=0.0, v_infinite_thresh=float('inf')):
+        super().__init__()
+        # SNN-related parameters
+        self.T = T
 
-    net = MonoSpikeFlowNetLike(tau=10, T=T, v_threshold=1.0, v_reset=0.0, v_infinite_thresh=1000).to(device)
+        # encoder layers (downsampling) for the left input spiketrain
+        self.conv1_left = nn.Sequential(
+            nn.Conv2d(in_channels=2, out_channels=32, kernel_size=3, stride=2, padding=(3-1)//2, bias=False),
+            nn.BatchNorm2d(32),
+            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
+        self.conv2_left = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=(3-1)//2, bias=False),
+            nn.BatchNorm2d(64),
+            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
+        self.conv3_left = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=(3-1)//2, bias=False),
+            nn.BatchNorm2d(128),
+            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
+        self.conv4_left = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=(3-1)//2, bias=False),
+            nn.BatchNorm2d(256),
+            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
 
-    frame = torch.rand(T, 1, 2, 360, 246).to(device)  # input tensor of shape [T, N_batches, 2 (polarities), W, H]
-    output = net(frame)
+        # encoder layers (downsampling) for the right input spiketrain
+        self.conv1_right = nn.Sequential(
+            nn.Conv2d(in_channels=2, out_channels=32, kernel_size=3, stride=2, padding=(3-1)//2, bias=False),
+            nn.BatchNorm2d(32),
+            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
+        self.conv2_right = nn.Sequential(
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=2, padding=(3-1)//2, bias=False),
+            nn.BatchNorm2d(64),
+            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
+        self.conv3_right = nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=(3-1)//2, bias=False),
+            nn.BatchNorm2d(128),
+            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
+        self.conv4_right = nn.Sequential(
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=(3-1)//2, bias=False),
+            nn.BatchNorm2d(256),
+            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
 
-    print("output.shape:", output.shape)  # output is a single-channel depth map of size [W, H]
+        # residual layers
+        self.conv_r11 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=(3-1)//2, bias=False),
+            nn.BatchNorm2d(512),
+            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
+        self.conv_r12 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=(3-1)//2, bias=False),
+            nn.BatchNorm2d(512),
+            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
+        self.conv_r21 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=(3-1)//2, bias=False),
+            nn.BatchNorm2d(512),
+            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
+        self.conv_r22 = nn.Sequential(
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=(3-1)//2, bias=False),
+            nn.BatchNorm2d(512),
+            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
 
-    assert output.shape[2:] == frame.shape[3:], \
-        "input and output do not have the same shape: respectively {} and {}".format(frame.shape[3:], output.shape[2:])
+        # decoder layers (upsampling)
+        self.deconv4 = nn.Sequential(
+            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
+        self.deconv3 = nn.Sequential(
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
+        self.deconv2 = nn.Sequential(
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
+        self.deconv1 = nn.Sequential(
+            nn.ConvTranspose2d(64, 1, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(1),
+            LIFNode(tau=tau, v_threshold=v_threshold, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
 
-    print("ok")
+        # these layers output depth maps at different scales, where depth is represented by the potential of IF neurons
+        # that do not fire ("I-neurons"), i.e., with an infinite threshold.
+        self.predict_depth = nn.Sequential(
+            IFNode(v_threshold=v_infinite_thresh, v_reset=v_reset, surrogate_function=surrogate.ATan()),
+        )
+
+    def detach(self):
+        for m in self.modules():
+            if isinstance(m, BaseNode):
+                m.v.detach_()
+            elif isinstance(m, layer.Dropout):
+                m.mask.detach_()
+
+    def forward(self, x_left, x_right):
+        # x_left and x_right must be of shape [batch_size, num_frames_per_depth_map, 2 (polarities), W, H]
+
+        for t in range(x_left.shape[1]):
+
+            # separate left and right input spiketrains
+            left_frame = x_left[:, t, :, :, :][0]
+            right_frame = x_right[:, t, :, :, :][0]
+
+            # pass through left encoder layers
+            out_conv1_left = self.conv1_left(left_frame)
+            out_conv2_left = self.conv2_left(out_conv1_left)
+            out_conv3_left = self.conv3_left(out_conv2_left)
+            out_conv4_left = self.conv4_left(out_conv3_left)
+
+            # pass through right encoder layers
+            out_conv1_right = self.conv1_right(right_frame)
+            out_conv2_right = self.conv2_right(out_conv1_right)
+            out_conv3_right = self.conv3_right(out_conv2_right)
+            out_conv4_right = self.conv4_right(out_conv3_right)
+
+            # concatenate the features calculated by both pathways
+            out_conv4 = torch.cat(out_conv4_left, out_conv4_right)
+
+            # pass through residual blocks
+            out_rconv11 = self.conv_r11(out_conv4)
+            out_rconv12 = self.conv_r12(out_rconv11) + out_conv4
+            out_rconv21 = self.conv_r21(out_rconv12)
+            out_rconv22 = self.conv_r22(out_rconv21) + out_rconv12
+
+            # gradually upsample while summing spikes and passing through skip connections
+            out_deconv4 = self.deconv4(out_rconv22)
+            out_add4 = self.crop_like(out_deconv4, out_conv3_left) + out_conv3_left + out_conv3_right
+
+            out_deconv3 = self.deconv3(out_add4)
+            out_add3 = self.crop_like(out_deconv3, out_conv2_left) + out_conv2_left + out_conv2_right
+
+            out_deconv2 = self.deconv2(out_add3)
+            out_add2 = self.crop_like(out_deconv2, out_conv1_left) + out_conv1_left + out_conv1_right
+
+            out_deconv1 = self.deconv1(out_add2)
+
+            # the membrane potentials of the output IF neuron carry the depth prediction
+            out_depth1 = self.predict_depth(out_deconv1)
+            return self.predict_depth[-1].v
 
