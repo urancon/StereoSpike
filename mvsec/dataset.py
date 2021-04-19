corrected epoch loss calculation, save best model, added more logs

mvsec/dataset.py
@@ -1,23 +1,24 @@
 import h5py
 import numpy as np
+import matplotlib.pyplot as plt
+import cv2
 import torch
 from torch.utils.data.dataset import Dataset
 import spikingjelly
-from spikingjelly.datasets.utils import EventsFramesDatasetBase, integrate_events_to_frames, normalize_frame
 
 from .utils import mvsecLoadRectificationMaps, mvsecRectifyEvents, mvsecCumulateSpikesIntoFrames, \
     mvsecSpikesAndDepth, mvsecToVideo
 from network.metrics import lin_to_log_depths
 
 
-SEQUENCES_FRAMES = {'indoor_flying': {'indoor_flying_1': (140, 1200),
+SEQUENCES_FRAMES = {'indoor_flying': {'indoor_flying_1': (140, 400),  # (140, 1200)
                                       'indoor_flying_2': (160, 1580),
                                       'indoor_flying_3': (125, 1815),
                                       'indoor_flying_4': (90, 360)}
                     }
 
 
-class MVSEC(EventsFramesDatasetBase):
+class MVSEC(Dataset):
     """
     Neuromorphic dataset class to hold MVSEC data.
 
@@ -33,21 +34,26 @@ class MVSEC(EventsFramesDatasetBase):
     def get_wh():
         return 346, 260
 
-    def __init__(self, root: str, num_frames_per_depth_map=1, normalization='max', take_log=True, show_sequence=False):
+    def __init__(self, root: str, num_frames_per_depth_map=1, normalization=None, take_log=True, show_sequence=False):
+        print("\n#####################################")
+        print("# LOADING AND PREPROCESSING DATASET #")
+        print("#####################################\n")
+
         self.root = root
         self.normalization = normalization
+        self.num_frames_per_depth_map = num_frames_per_depth_map
 
         # load the data
-        datafile = self.root + 'indoor_flying/indoor_flying4_data'
+        datafile = self.root + 'indoor_flying/indoor_flying1_data'
         data = h5py.File(datafile + '.hdf5', 'r')
-        datafile_gt = self.root + 'indoor_flying/indoor_flying4_gt'
+        datafile_gt = self.root + 'indoor_flying/indoor_flying1_gt'
         data_gt = h5py.File(datafile_gt + '.hdf5', 'r')
 
         # get the ground-truth depth maps (i.e. our labels) and their timestamps
         Ldepths_rect = np.array(data_gt['davis']['left']['depth_image_rect'])  # RECTIFIED / LEFT
         Rdepths_rect = np.array(data_gt['davis']['right']['depth_image_rect'])  # RECTIFIED / RIGHT
         Ldepths_rect_ts = np.array(data_gt['davis']['left']['depth_image_rect_ts'])
-        Rdepths_rect_ts = np.array(data_gt['davis']['right']['depth_image_rect_ts'])  # TODO: Right depth map might be unnecessary !!!
+        Rdepths_rect_ts = np.array(data_gt['davis']['right']['depth_image_rect_ts'])
 
         # convert linear (metric) to normalized log depths if required
         if take_log:
@@ -55,7 +61,7 @@ class MVSEC(EventsFramesDatasetBase):
             Rdepths_rect = lin_to_log_depths(Rdepths_rect)
 
         # remove depth maps occurring during take-off and landing of the drone (bad data)
-        start_idx, end_idx = SEQUENCES_FRAMES['indoor_flying']['indoor_flying_4']
+        start_idx, end_idx = SEQUENCES_FRAMES['indoor_flying']['indoor_flying_1']
         Ldepths_rect = Ldepths_rect[start_idx:end_idx, :, :]
         Rdepths_rect = Rdepths_rect[start_idx:end_idx, :, :]
         Ldepths_rect_ts = Ldepths_rect_ts[start_idx:end_idx]
@@ -95,8 +101,9 @@ class MVSEC(EventsFramesDatasetBase):
         assert xL.shape == xR.shape
 
         if self.normalization is not None and self.normalization != 'frequency':
-            self.data_left = normalize_frame(xL, self.normalization)
-            self.data_right = normalize_frame(xR, self.normalization)
+            raise NotImplementedError(self.normalization)
+            #self.data_left = normalize_frame(xL, self.normalization)
+            #self.data_right = normalize_frame(xR, self.normalization)
         else:
             self.data_left = xL
             self.data_right = xR
@@ -123,6 +130,22 @@ class MVSEC(EventsFramesDatasetBase):
         label = self.labels[index]
         return [frame_left, frame_right], label
 
+    def show(self):
+        # TODO: les 2 premières frames de chaque chunk ont des pixels a 0.5, tandis que les suivantes en ont à 1 et à 2
+        #  determiner pourquoi !! Indice: normalisation de Wei (ligne 100 de ce fichier).
+        i = 0
+        for chunk in self.data_left:
+            i += 1
+            j = 0
+            for frame in chunk:
+                j +=1
+                f = frame[0]
+                #plt.imshow(f)
+                #plt.show()
+                cv2.imshow("cumulated ON event frames", f)
+                cv2.waitKey(int(1000 / (20 * self.num_frames_per_depth_map)))
+        cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     print(MVSEC.get_wh())
@@ -131,3 +154,4 @@ if __name__ == "__main__":
     x, y = dataset[0]
     print(x[0].shape)
     print(y.shape)
+

tbptt.py
@@ -55,7 +55,16 @@ class ApproximatedTBPTT:
 
     def train(self, train_data_loader, nfpdm=5, N=1):
 
+        print("\n################")
+        print("#   TRAINING   #")
+        print("################\n")
+
         logfile = open("./checkpoints/training_logs.txt", "w+")
+
+        print("\nTraining with approximated TBPTT procedure: {} frames between labels, k1=1, k2=N*K1, N={}\n".format(nfpdm, N))
+        logfile.write("Training with approximated TBPTT procedure: {} frames between labels, k1=1, k2=N*K1, N={}\n\n".format(nfpdm, N))
+
+        min_MDE = 100.0  # arbitrarily large value to account for worst performances at initialization
 
         for epoch in range(self.n_epochs):
             start_time = time.time()
@@ -99,7 +108,7 @@ class ApproximatedTBPTT:
                     # the others remain as they are
                     out_depth_potentials, label_batch = mask_dead_pixels(out_depth_potentials, label_batch)
 
-                    loss = self.loss_module(out_depth_potentials, label)
+                    loss = self.loss_module(out_depth_potentials, label_batch)
                     #loss = F.mse_loss(out_depth_potentials, label)
                     #loss = Total_Loss(out_depth_potentials, label, alpha=0.)
                     #loss = MeanDepthError(out_depth_potentials, label_batch)
@@ -117,56 +126,28 @@ class ApproximatedTBPTT:
                     chunk_batch = []
                     label_batch = []
 
-            epoch_loss = running_loss / (len(train_data_loader) / self.n_epochs)
+            epoch_loss = running_loss / len(train_data_loader)
 
             functional.reset_net(self.one_chunk_module)
 
-            chunk_sequence = []
-            label_sequence = []
-            chunk_batch = []
-            label_batch = []
-
+            # for evaluation, processes the sequence chunk by chunk
             self.one_chunk_module.eval()
             with torch.no_grad():
-                for i, (chunk, label) in enumerate(tqdm(train_data_loader)):
+                for chunk, label in tqdm(train_data_loader):
 
                     chunk = chunk[0].to(device=self.device,
-                                        dtype=torch.float)  # only take left DVS data for monocular depth estimation
+                                        dtype=torch.float)
                     label = label.to(self.device)
 
-                    chunk_sequence.append(chunk)
-                    label_sequence.append(label)
+                    out_depth_potentials = self.one_chunk_module(chunk)
 
-                    if (i + 1) % (2 * N - 1) == 0:
+                    out_depth_potentials, label = mask_dead_pixels(out_depth_potentials, label)
 
-                        # the batch dimension is equal to N, so a batch is composed of N shifted chunk sequences
-                        # e.g. N=3, chunk_sequence=[0, 1, 2, 3, 4] (len: 5=2N-1)
-                        # --> chunk_batch=[[0, 1, 2], [1, 2, 3], [2, 3, 4]]
-                        # --> label_batch=[2, 3, 4]
-                        for j in range(N):
-                            chunk_batch.append(torch.cat(chunk_sequence[j:N + j], dim=1))  # concatenate chunks along frame (time) dimension  [1, nfpdm, 2, H, W] --> [1, N*nfpdm, 2, H, W]
-                            label_batch.append(label_sequence[N-1+j])  # get the label corresponding to the chunk sequence --> [1, H, W]
-                        chunk_batch = torch.cat(chunk_batch, dim=0)  # concatenate along batch dimension --> [N, N*nfpdm, 2, H, W]
-                        label_batch = torch.cat(label_batch, dim=0)  # concatenate along batch dimension --> [N, H, W]
+                    self.one_chunk_module.detach()
 
-                        # the batches are ready now, train on them
-                        out_depth_potentials = self.one_chunk_module(chunk_batch)
+                    running_MDE += MeanDepthError(out_depth_potentials, label)
 
-                        # mask all invalid pixels, corresponding to pixels of the groundtruth with values of 0 or 255
-                        # these pixels become 0 in both prediction and groundtruth (so that residuals become 0 too),
-                        # the others remain as they are
-                        out_depth_potentials, label_batch = mask_dead_pixels(out_depth_potentials, label_batch)
-
-                        self.one_chunk_module.detach()
-
-                        running_MDE += MeanDepthError(out_depth_potentials, label)
-
-                        chunk_sequence = []
-                        label_sequence = []
-                        chunk_batch = []
-                        label_batch = []
-
-                    epoch_MDE = running_MDE / len(train_data_loader)
+                epoch_MDE = running_MDE / len(train_data_loader)
 
             end_time = time.time()
             epoch_summary = "Epoch: {}, Loss: {}, Mean Depth Error: {}, Time: {}\n".format(epoch, epoch_loss,
@@ -175,8 +156,11 @@ class ApproximatedTBPTT:
             print(epoch_summary)
             logfile.write(epoch_summary)
 
-        # save model
-        torch.save(self.one_chunk_module.state_dict(), "./checkpoints/spikeflownet_snn.pth")
+            # save model if better results
+            if epoch_MDE < min_MDE:
+                print("Best performances so far: saving model...\n")
+                torch.save(self.one_chunk_module.state_dict(), "./checkpoints/spikeflownet_snn.pth")
+                min_MDE = epoch_MDE
 
         # close log file
         logfile.close()
@@ -184,14 +168,14 @@ class ApproximatedTBPTT:
 
 if __name__ == "__main__":
     from mvsec import MVSEC
-    from network.models import SpikeFlowNetLike
+    from network.models import SpikeFlowNetLike, SpikeFlowNetLike_cext
 
     device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 
     nfpdm = 5  # (!) don't choose it too big because of memory limitations (!)
     N = 1
     learning_rate = 0.0001
-    n_epochs = 10
+    n_epochs = 15
 
     dataset = MVSEC('/home/ulysse/Desktop/PFE CerCo/datasets/MVSEC/',
                     num_frames_per_depth_map=nfpdm,
@@ -205,7 +189,7 @@ if __name__ == "__main__":
                                                     drop_last=False,
                                                     pin_memory=True)
 
-    net = SpikeFlowNetLike(tau=10.,
+    net = SpikeFlowNetLike_cext(tau=10.,
                            v_threshold=1.0,
                            v_reset=0.0,
                            v_infinite_thresh=float('inf'),
@@ -221,5 +205,4 @@ if __name__ == "__main__":
     runner.train(train_data_loader, nfpdm=nfpdm, N=N)
 
     print("done")
-
 