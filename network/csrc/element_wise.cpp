fad211135c576b8349ccc3f0376ca5ab0256f703
urancon <ulysse.rancon@gmail.com>
2021-07-19 09:53:52 +0200

    removed C++ and CUDA PLIF codes, now available on spikingjelly by default

--- network/csrc/element_wise.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <torch/extension.h>
-#include <math.h>
-
-torch::Tensor spikes_or(torch::Tensor & x, torch::Tensor & y);
-
-PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("spikes_or", &spikes_or);
-}
