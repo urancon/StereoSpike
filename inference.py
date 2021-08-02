be74842168f2055744417b8d8c2f9ffd37a11cf6
urancon <ulysse.rancon@gmail.com>
2021-08-02 13:40:37 +0200

    cleaning repo

--- inference.py
+++ inference.py
@@ -1,77 +1,154 @@
+import time
 from tqdm import tqdm
 import numpy as np
-import cv2
+from PIL import Image
 import matplotlib.pyplot as plt
-
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.tensorboard import SummaryWriter
+from torchvision import transforms
+import torchvision.transforms.functional as F
+
 from spikingjelly.clock_driven import functional
-from spikingjelly import visualizing
 
-from mvsec import MVSEC
-from network import SpikeFlowNetLike, FusionFlowNetLike
-from network import MeanDepthError, Total_Loss
+from network.metrics import MeanDepthError, Total_Loss, mask_dead_pixels
 
+from datasets.MVSEC import MVSEC, shuffled_MVSEC, binocular_shuffled_MVSEC
+from datasets.data_augmentation import ToTensor, RandomHorizontalFlip, RandomTimeMirror, RandomEventDrop
+from network.ANN_models import Analog_ConvLSTM_SpikeFlowNetLike, \
+    concat_Analog_ConvLSTM_SpikeFlowNetLike, \
+    multiscale_concat_Analog_ConvLSTM_SpikeFlowNetLike, \
+    biased_multiscale_concat_Analog_ConvLSTM_SpikeFlowNetLike, \
+    attention_biased_multiscale_concat_Analog_ConvLSTM_SpikeFlowNetLike, \
+    binocular_multiscale_concat_Analog_ConvLSTM_SpikeFlowNetLike, \
+    biased_binocular_multiscale_concat_Analog_ConvLSTM_SpikeFlowNetLike
+from viz import show_learning
 
 device = torch.device('cuda:0') if torch.cuda.is_available() else torch.device('cpu')
 
-outfile = './checkpoints/prediction_vs_groundtruth.mp4'
-nfpdm = 5
-LIDAR_FPS = 20
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter(outfile, fourcc, LIDAR_FPS, (2*346, 260), isColor=False)
-predicted_frames_list = []
-label_list = []
 
-T = 10  # (!) don't choose T too big because of memory limitations (!)
-batch_size = 1
+###########################
+# VISUALIZATION FUNCTIONS #
+###########################
 
-dataset = MVSEC('/home/ulysse/Desktop/PFE CerCo/datasets/MVSEC/',
-                num_frames_per_depth_map=nfpdm,
-                normalization='max',
-                take_log=True,
-                show_sequence=False)
-train_data_loader = torch.utils.data.DataLoader(dataset=dataset,
-                                                batch_size=batch_size,
-                                                shuffle=False,
-                                                num_workers=0,
-                                                drop_last=False,
-                                                pin_memory=True)
-#dataset.show()
-
-for chunk, label in train_data_loader:
-    chunk = chunk[0].squeeze()
-    for frame in chunk:
-        f = frame[0]
-        plt.imshow(f)
-        plt.show()
+plt.ion()
+fig = plt.figure()
+png_dir = "./results/visualization/pngs/"
 
 
-net = SpikeFlowNetLike(tau=10, T=T, v_threshold=1.0, v_reset=0.0, v_infinite_thresh=float('inf')).to(device)
-net.load_state_dict(torch.load("./checkpoints/spikeflownet_snn.pth"))
-net.eval()
+######################
+# GENERAL PARAMETERS #
+######################
 
-torch.no_grad()
+nfpdm = 2  # (!) don't choose it too big because of memory limitations (!)
+batchsize = 1
+take_log = False
+learning_rate = 0.001
+weight_decay = 0.0
+n_epochs = 20
+show = True
 
-# inference on all frames
-for chunk, label in tqdm(train_data_loader):
-    chunk = chunk[0].to(device=device, dtype=torch.float)  # only take left DVS data for monocular depth estimation
-    label = ((label / label.max()) * 255).detach().cpu().numpy().astype('uint8')
-    label_list.append(np.squeeze(label))
-    prediction = net(chunk)
-    predicted_frames_list.append(torch.squeeze(prediction).cpu().detach().numpy())
-    net.detach()
 
-# constitution of a comparison video file
-for f, j in zip(predicted_frames_list, range(len(label_list))):
-    f = ((f / f.max()) * 255).astype('uint8')  # normalization of pixel values
-    f = np.concatenate((f, label_list[j]), axis=1)  # concatenate to show left and right images side by side
-    #f = cv2.applyColorMap(f, cv2.COLORMAP_JET)
-    cv2.imshow("prediction_vs_groundtruth", f)
-    out.write(f)
-    cv2.waitKey(int(1000 / LIDAR_FPS))
+########
+# DATA #
+########
 
-out.release()
-print("saved video sequence to 'prediction_vs_groundtruth.mp4'...\n")
+# random transformations for data augmentation
+tsfm = []
 
+test_shuffled_dataset = binocular_shuffled_MVSEC('/home/ulysse/Desktop/PFE CerCo/datasets/MVSEC/',
+                                       scenario='indoor_flying', case='1',
+                                       num_frames_per_depth_map=nfpdm, warmup_chunks=5, train_chunks=5,
+                                       mirror_time=False, take_log=take_log, show_sequence=False)
+test_data_loader = torch.utils.data.DataLoader(dataset=test_shuffled_dataset,
+                                               batch_size=1,
+                                               shuffle=False,
+                                               drop_last=False,
+                                               pin_memory=True)
+
+
+###########
+# NETWORK #
+###########
+
+net = binocular_multiscale_concat_Analog_ConvLSTM_SpikeFlowNetLike().to(device)
+net.load_state_dict(torch.load('./results/checkpoints/shuffled_ann_binocular/spikeflownet_snn.pth'))
+
+################
+# OPTIMIZATION #
+################
+
+optimizer = torch.optim.Adam(net.parameters(), lr=learning_rate, weight_decay=weight_decay)
+scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10, 20], gamma=0.1)
+loss_module = Total_Loss
+
+logfile = open("./results/checkpoints/training_logs.txt", "w+")
+
+
+############
+# TRAINING #
+############
+
+for epoch in range(n_epochs):
+
+    start_time = time.time()
+    running_test_loss = 0
+    running_test_MDE = 0
+
+    i=0
+
+    net.eval()
+    with torch.no_grad():
+        start_time = time.time()
+        for init_pots, warmup_chunks_left, warmup_chunks_right, test_chunks_left, test_chunks_right, label in tqdm(test_data_loader):
+
+            warmup_chunks_left = warmup_chunks_left.view(1, warmup_chunks_left.shape[1] * warmup_chunks_left.shape[2], 2, 260, 346).to(device, dtype=torch.float)
+            #warmup_chunks_right = warmup_chunks_right.view(1, warmup_chunks_right.shape[1] * warmup_chunks_right.shape[2], 2, 260, 346).to(device, dtype=torch.float)
+            warmup_chunks_right = torch.zeros_like(warmup_chunks_left)
+            test_chunks_left = test_chunks_left.view(1, test_chunks_left.shape[1] * test_chunks_left.shape[2], 2, 260, 346).to(device, dtype=torch.float)
+            #test_chunks_right = test_chunks_right.view(1, test_chunks_right.shape[1] * test_chunks_right.shape[2], 2, 260, 346).to(device, dtype=torch.float)
+            test_chunks_right = torch.zeros_like(test_chunks_left)
+            init_pots = init_pots.to(device)
+            label = label.to(device)
+
+            functional.reset_net(net)
+            net.reset_convLSTM_states()
+
+            net(warmup_chunks_left, warmup_chunks_right)
+
+            pred = net(test_chunks_left, test_chunks_right)[0]  # only take full scale prediction in evaluation
+
+            if show:
+                title = 'EPOCH {} - {}'.format(epoch, 'TEST')
+                viz = show_learning(fig, test_chunks_right, pred, label, 'eval')
+                viz = Image.fromarray(viz)
+                viz.save(png_dir + "epoch{}_chunk{}_1.png".format(epoch, i))
+
+            loss = loss_module(pred, label)
+
+            net.detach()
+
+            running_test_loss += loss.item() / test_chunks_left.size(0)
+            running_test_MDE += MeanDepthError(pred, label)
+
+            i += 1
+
+    epoch_test_loss = running_test_loss / len(test_data_loader)
+    epoch_test_MDE = running_test_MDE / len(test_data_loader)
+    epoch_test_time = time.time() - start_time
+    test_epoch_summary = "Epoch: {}, Test Loss: {}, Test Mean Depth Error: {}, Time: {}\n".format(epoch,
+                                                                                                  epoch_test_loss,
+                                                                                                  epoch_test_MDE,
+                                                                                                  epoch_test_time)
+    print(test_epoch_summary)
+    logfile.write(test_epoch_summary)
+
+    # save model if better results
+    if epoch_test_MDE < net.get_max_accuracy():
+        print("Best performances so far: saving model...\n")
+        torch.save(net.state_dict(), "./results/checkpoints/spikeflownet_snn.pth")
+        net.update_max_accuracy(epoch_test_MDE)
+
+    net.increment_epoch()
+
+    scheduler.step()
